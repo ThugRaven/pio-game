f8ae71528163c20753ab7a130dabf9fff334dd3a
ThugRaven <kamilo1715@wp.pl>
2020-12-07 10:32:17 +0100

    Add Player methods in Game

--- src/com/thugraven/Game.java
+++ src/com/thugraven/Game.java
@@ -1,21 +1,81 @@
 package com.thugraven;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 public class Game {
     private List<Player> players = new ArrayList<>();
+    private Random rand = new Random();
 
     public Game(){
 
     }
 
     public void addPlayer(Player player){
-        players.add(player);
+        if(!nameExists(player.getName())){
+            players.add(player);
+        }
+        else{
+            player.setName(findUniqueName(player.getName()));
+            addPlayer(player);
+            //throw new IllegalArgumentException("This name already exist!");
+        }
+    }
+
+    public void removePlayer(String name){
+        // 1
+//        for(int i = 0; i < players.size(); i++){
+//            if(players.get(i).getName().equals(name)){
+//                players.remove(i);
+//                break;
+//            }
+//        }
+
+        // 2
+//        for(Player player: players){
+//            if(player.getName().equals(name)){
+//                players.remove(player);
+//                break;
+//            }
+//        }
+
+        // 3
+        Iterator<Player> it = players.iterator();
+        while(it.hasNext()){
+            if(it.next().getName().equals(name)){
+                it.remove();
+                break;
+            }
+        }
+
+        // 4
+//        players.removeIf(player -> player.getName().equals(name));
+    }
+
+    public void printPlayers(){
+        for(Player player: players){
+            System.out.println(player.getName());
+        }
+    }
+
+    public boolean nameExists(String name){
+        for(Player player: players){
+            if(player.getName().equals(name)){
+                //System.err.println(player.getName() + " == " + name);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String findUniqueName(String name){
+        return name + rand.nextInt(10) + 1; // range of 1 - 10
     }
 
     public void play(){
-        if(players.isEmpty()) {
+        if(players.isEmpty()){
             throw new IllegalStateException("You cannot start a game without any players!");
         }
 
--- src/com/thugraven/PioGame.java
+++ src/com/thugraven/PioGame.java
@@ -19,38 +19,17 @@ public class PioGame {
             player.setName("Ziutek");
 
             Game game = new Game();
-            //game.addPlayer(player);
-//            game.addPlayer(new PlayerComp("Bot123"));
-//            game.addPlayer(new PlayerComp("Ziutek"));
+            game.addPlayer(player);
+            game.addPlayer(new PlayerComp("Bot123"));
+            game.addPlayer(new PlayerComp("Ziutek"));
+
+            //game.removePlayer("Ziutek");
+
+            game.printPlayers();
             game.play();
 //        }
 //        catch(IllegalArgumentException e){
 //            System.err.println(e.getMessage());
-//        }
-
-
-
-//        int randNum;
-//        int playerNum;
-//        int count = 1;
-//
-//        while(true){
-//            randNum = rand.nextInt(6) + 1;
-//            System.out.println("Picking a number...\nDice: " + randNum);
-//
-////            playerNum = rand.nextInt(6) + 1;
-//            playerNum = player.guess();
-//
-//            System.out.println("Player " + player.getName() + ": " + playerNum);
-//
-//            if(playerNum == randNum){
-//                System.out.println("You won! Tries: " + count);
-//                break;
-//            }
-//            else{
-//                System.out.println("You lost! Try again\n");
-//                count++;
-//            }
 //        }
     }
 }
